79be60320d5624ea5b6604434553ad710ae63ce4
Benjamin Idewor <benjaminidewor@gmail.com>
2021-12-17 20:29:52 +0100

    feat: connect to DB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const connectDB = require('./config/db');
+
+connectDB();
+
+const PORT = process.env.PORT || 8000;
+// json parser
+app.use(express.json());
+
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log(`Server running on port ${PORT}`);
 });
